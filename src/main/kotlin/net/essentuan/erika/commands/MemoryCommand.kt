010d811f13f55e850c54cfd96f19037a7062ef5c
Essentuan <51426319+Essentuan@users.noreply.github.com>
2024-08-25 03:15:46 -0500

    Add 'memory dump'

--- src/main/kotlin/net/essentuan/erika/commands/MemoryCommand.kt
+++ src/main/kotlin/net/essentuan/erika/commands/MemoryCommand.kt
@@ -5,10 +5,12 @@ package net.essentuan.erika.commands
 import com.essentuan.acf.core.annotations.Command
 import com.essentuan.acf.core.annotations.Subcommand
 import com.mojang.brigadier.context.CommandContext
+import com.sun.management.HotSpotDiagnosticMXBean
 import net.essentuan.erika.framework.console.Logging
 import net.essentuan.erika.framework.db.`object`.Memory
 import net.essentuan.erika.framework.formatting.table
 import net.essentuan.esl.reflections.extensions.simpleString
+import java.lang.management.ManagementFactory
 
 var MEMORY_DEBUG_ENABLED: Boolean = false
 
@@ -52,4 +54,31 @@ private fun CommandContext<*>.counts() {
 @Subcommand("gc")
 private fun CommandContext<*>.gc() {
     Runtime.getRuntime().gc()
+}
+
+@Subcommand("dump")
+private fun CommandContext<*>.dump() {
+    Logging.info("Dumping heap")
+    Logging.info("Acquiring MBeanServer")
+
+    val server = ManagementFactory.getPlatformMBeanServer()
+
+    Logging.info("Creating dump")
+
+    try {
+        val file = "./heap-${System.currentTimeMillis()}.hprof"
+
+        ManagementFactory.newPlatformMXBeanProxy(
+            server,
+            "com.sun.management:type=HotSpotDiagnostic",
+            HotSpotDiagnosticMXBean::class.java
+        ).dumpHeap(
+            file,
+            true
+        )
+
+        Logging.info("Heap dump has been created at $file!")
+    } catch (ex: Exception) {
+        Logging.error("Error creating dump!", ex)
+    }
 }
