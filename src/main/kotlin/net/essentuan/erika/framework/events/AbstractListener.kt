beb47a62f1cd57d42a2de6f3c05d8cf14d751877
Essentuan <51426319+Essentuan@users.noreply.github.com>
2024-09-20 00:15:44 -0500

    Make event final in AbstractListener

--- src/main/kotlin/net/essentuan/erika/framework/events/AbstractListener.kt
+++ src/main/kotlin/net/essentuan/erika/framework/events/AbstractListener.kt
@@ -4,18 +4,22 @@ import net.essentuan.erika.framework.events.api.Listener
 import net.essentuan.esl.Rating
 import net.essentuan.esl.reflections.extensions.extends
 
-abstract class AbstractListener<T: Event>(
+abstract class AbstractListener<T : Event>(
     override val id: String,
-    override val event: Class<T>,
+    final override val event: Class<T>,
     override val phase: Rating,
     val receiveCancelled: Boolean = false,
     val filter: Class<*> = Any::class.java
 ) : Listener<T> {
     val list: Listener.List<T> = Event.Bus[event]
 
-    override fun listen() { list+= this }
+    override fun listen() {
+        list += this
+    }
 
-    override fun close() { list.remove(this) }
+    override fun close() {
+        list.remove(this)
+    }
 
     override fun invoke(event: T) {
         if ((!event.cancelled || receiveCancelled) && (event !is GenericEvent<*> || event.type extends filter))
